d3b5dda1f9a721e736b61f34b450e52a38965f96
CellBody <austintingwork@gmail.com>
2016-10-07 14:03:49 +0800

    Post

--- app/src/main/java/com/expixel/binki/MainActivity.java
+++ app/src/main/java/com/expixel/binki/MainActivity.java
@@ -1,31 +1,49 @@
 package com.expixel.binki;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.OnItemClickListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+
+    @BindView(R.id.recyclerView_main)
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        // 讓列表資料反轉 THIS ALSO SETS setStackFromBottom to true
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
     }
 
     @Override
@@ -49,4 +67,50 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @OnClick(R.id.fab)
+    public void onClick() {
+        DialogPlus dialogPuls = DialogPlus.newDialog(this)
+                .setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"asdfa"}))
+                .setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(DialogPlus dialog, Object item, View view, int position) {
+                    }
+                })
+                .setExpanded(true)  // This will enable the expand feature, (similar to android L share dialog)
+                .create();
+        dialogPuls.show();
+    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        FirebaseRecyclerAdapter<Event, ItemViewHolder> adapter =
+//                new FirebaseRecyclerAdapter<Event, ItemViewHolder>(
+//                        Event.class,
+//                        ItemViewHolder.layoutResId,
+//                        ItemViewHolder.class,
+//                        dbRef.child("post")
+//                ) {
+//                    @Override
+//                    protected void populateViewHolder(ItemViewHolder viewHolder, Event event, int position) {
+//                        ImageLoader.getInstance().displayImage(event.getUserImgUrl(), viewHolder.userImage);
+//                        //  取得這個item的key
+//                        getRef(i).getKey()
+//                    }
+//                };
+//        recyclerView.setAdapter(adapter);
+//    }
+//
+//
+//    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+//
+//        public final static int layoutResId = R.layout.ac_main_item;
+//        CircleImageView userImg;
+//
+//        public ItemViewHolder(View view) {
+//            super(view);
+//            userImg = (CircleImageView) view.findViewById(R.id.userImage);
+//        }
+//}
 }
