0f558ed9f08cfc2a1cc03a05f962767364be17ae
AustinTing <austintingwork@gmail.com>
2016-11-01 15:06:59 +0800

    Merge from remote

--- app/src/main/java/com/expixel/binki/MainActivity.java
+++ app/src/main/java/com/expixel/binki/MainActivity.java
@@ -598,8 +598,6 @@ public class MainActivity extends BaseActivity {
             imgUser = (CircleImageView) view.findViewById(R.id.imgUser_main);
             userName = (TextView) view.findViewById(R.id.userName_main);
             bookName = (TextView) view.findViewById(R.id.bookName_main);
-            btnLike = (Button) view.findViewById(R.id.btnLike_main);
-            btnHide = (Button) view.findViewById(R.id.btnHide_main);
         }
     }
 
